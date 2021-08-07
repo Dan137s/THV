5e25f42232bdc50692d6aaf5f3bea84b17f6a87e
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-08-07 19:32:59 -0400

    menu redireccion login 4

--- Login/index.php
+++ Login/index.php
@@ -32,14 +32,15 @@
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-				<form class="login100-form validate-form">
+				
 					<span class="login100-form-title p-b-49">
 						INICIAR SESIÓN
 					</span>
 
 					<div class="wrap-input100 validate-input m-b-23" data-validate = "Escriba o verifique #usuario">
 						<span class="label-input100">Usuario</span>
-						<input class="input100" type="text" name="username" placeholder="Escriba el usuario" id="txt_usu">
+						<input class="input100" type="text" name="username" placeholder="Escriba el usuario" id="txt_usu" 
+						autocomplete="new-password">
 						<span class="focus-input100" data-symbol="&#xf206;"></span>
 					</div>
 
@@ -58,7 +59,7 @@
 					<div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn">
+							<button class="login100-form-btn" onclick="VerificarUsuario()">
 								ENTRAR
 							</button>
 						</div>
@@ -78,7 +79,7 @@
 						</a>
 					</div>
 
-				</form>
+				
 			</div>
 		</div>
 	</div>
@@ -86,6 +87,9 @@
 
 	<div id="dropDownSelect1"></div>
 	
+<!--===============================================================================================-->
+	<script src="vendor/sweetalert2/sweetalert2.js"></script>
+<!--===============================================================================================-->
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
@@ -102,6 +106,10 @@
 	<script src="vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
 	<script src="js/main.js"></script>
+	<script src="../js/usuario.js"></script>
 
 </body>
+<<script>
+txt_usu.focus();
+</script>
 </html>
---
+++ js/usuario.js
@@ -0,0 +1,9 @@
+function VerificarUsuario() {
+    var usu = $("#txt_usu").val();
+    var con = $("#txt_con").val();
+
+    if (usu.length == 0 || con.length == 0) {
+        return Swal.fire("Mensaje de advertencia", "Llene los campos vacios", "warning");
+    }
+
+}
