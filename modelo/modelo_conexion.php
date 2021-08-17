8e616c7c564c8f1aa5435625ef52a43d1361e6ac
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-08-17 17:43:23 -0400

    modificacion 29

--- modelo/modelo_conexion.php
+++ modelo/modelo_conexion.php
@@ -6,10 +6,18 @@
 		private $basedatos;
 		public $conexion;
 		public function __construct(){
+
+			//Para servidor local
 		    $this->servidor = "localhost";
 			$this->usuario = "root";
 			$this->contrasena = "";
 			$this->basedatos = "thv_bd";
+			
+			//Para el servidor de prueba
+			//$this->servidor = "sql200.epizy.com";
+			//$this->usuario = "epiz_29366167";
+			//$this->contrasena = "GkQpBTrRVn0";
+			//$this->basedatos = "epiz_29366167_thv_bd";
 		}
 		function conectar(){
 			$this->conexion = new mysqli($this->servidor,$this->usuario,$this->contrasena,$this->basedatos);
