6236322b542aa6bc6a444278d665b5a5c76edb63
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-08-17 18:12:36 -0400

    modificacion 30

--- vista/index.php
+++ vista/index.php
@@ -80,127 +80,117 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- Messages: style can be found in dropdown.less-->
+         
+         
+          <!-- Notifications: administrador -->  
+          <?php
+          if($_SESSION['S_ROL']=='ADMINISTRADOR'){
 
-          <!-- Notifications: style can be found in dropdown.less -->
+          ?>
+        
           <li class="dropdown notifications-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-bell-o"></i>
-              <span class="label label-warning">10</span>
+              <span class="label label-danger">3</span>
             </a>
             <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
+              <li class="header">Hola Tienes (+3) Nuevas Notificaciones</li>
               <li>
                 <!-- inner menu: contains the actual data -->
-                <ul class="menu">
+               <ul class="menu">
                   <li>
                     <a href="#">
-                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                      <i class="fa fa-user text-black"></i> Solicitud Vecino 12.345.678-9
                     </a>
                   </li>
                   <li>
                     <a href="#">
-                      <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-                      page and may cause design problems
+                      <i class="fa fa-user text-red"></i> Solicitud Vecino 12.345.678-9
                     </a>
                   </li>
                   <li>
                     <a href="#">
-                      <i class="fa fa-users text-red"></i> 5 new members joined
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-shopping-cart text-green"></i> 25 sales made
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-user text-red"></i> You changed your username
+                      <i class="fa fa-user text-red"></i> Solicitud Vecino 12.345.678-9
                     </a>
                   </li>
                 </ul>
               </li>
-              <li class="footer"><a href="#">View all</a></li>
+              <li class="footer"><a href="#">Ver todo</a></li>
             </ul>
           </li>
-          <!-- Tasks: style can be found in dropdown.less -->
-          <li class="dropdown tasks-menu">
+          <?php 
+          }
+          ?>
+            <?php
+          if($_SESSION['S_ROL']=='ADMINISTRADOR'){
+
+          ?>
+           <?php 
+          }
+          ?>
+
+          <!-- Notifications: trabajador -->
+          <?php
+          if($_SESSION['S_ROL']=='TRABAJADOR'){
+
+          ?>
+          <!-- Notifications: trabajador -->
+          <li class="dropdown notifications-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-flag-o"></i>
-              <span class="label label-danger">9</span>
+              <i class="fa fa-bell-o"></i>
+              <span class="label label-danger">8</span>
             </a>
             <ul class="dropdown-menu">
-              <li class="header">You have 9 tasks</li>
+              <li class="header">Hola Tienes (+8) Nuevas Notificaciones</li>
               <li>
                 <!-- inner menu: contains the actual data -->
-                <ul class="menu">
-                  <li><!-- Task item -->
+               <ul class="menu">
+                  <li>
                     <a href="#">
-                      <h3>
-                        Design some buttons
-                        <small class="pull-right">20%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">20% Complete</span>
-                        </div>
-                      </div>
+                      <i class="fa fa-user text-black"></i> Solicitud Vecino 12.345.678-9
                     </a>
                   </li>
-                  <!-- end task item -->
-                  <li><!-- Task item -->
+                  <li>
                     <a href="#">
-                      <h3>
-                        Create a nice theme
-                        <small class="pull-right">40%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-green" style="width: 40%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">40% Complete</span>
-                        </div>
-                      </div>
+                      <i class="fa fa-user text-green"></i> Solicitud Vecino 12.345.678-9
                     </a>
                   </li>
-                  <!-- end task item -->
-                  <li><!-- Task item -->
+                  <li>
                     <a href="#">
-                      <h3>
-                        Some task I need to do
-                        <small class="pull-right">60%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-red" style="width: 60%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">60% Complete</span>
-                        </div>
-                      </div>
+                      <i class="fa fa-user text-black"></i> Solicitud Vecino 12.345.678-9
                     </a>
                   </li>
-                  <!-- end task item -->
-                  <li><!-- Task item -->
+                  <li>
                     <a href="#">
-                      <h3>
-                        Make beautiful transitions
-                        <small class="pull-right">80%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-yellow" style="width: 80%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">80% Complete</span>
-                        </div>
-                      </div>
+                      <i class="fa fa-user text-black"></i> Solicitud Vecino 12.345.678-9
                     </a>
                   </li>
-                  <!-- end task item -->
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-user text-red"></i> Solicitud Vecino 12.345.678-9
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-user text-red"></i> Solicitud Vecino 12.345.678-9
+                    </a>
+                  </li>
                 </ul>
               </li>
-              <li class="footer">
-                <a href="#">View all tasks</a>
-              </li>
+              <li class="footer"><a href="#">Ver todo</a></li>
             </ul>
           </li>
+          <?php 
+          }
+          ?>
+            <?php
+          if($_SESSION['S_ROL']=='TRABAJADOR'){
+
+          ?>
+           <?php 
+          }
+          ?>
+        
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
