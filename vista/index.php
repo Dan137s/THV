ef6a02275fe1c7f0e05802cbe117dd1397f93b06
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-08-17 18:49:44 -0400

    modificacion 33

--- vista/index.php
+++ vista/index.php
@@ -301,7 +301,7 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
           </a>
           <ul class="treeview-menu">
             <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_usuario_listar.php')"><i class="fa fa-address-book-o"></i> Usuarios</a></li>
-            <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_hospederia_listar.php')"><i class="fa fa-address-book-o"></i> Hospederia</a></li>
+            <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_hospederia_listar.php')"><i class="fa fa-file-text-o"></i> Hospederia</a></li>
             <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
             <li><a href = #><i class="fa fa-file-text-o"></i> Materiales</a></li>
             <li><a href = #><i class="fa fa-file-text-o"></i> Herramientas</a></li>
@@ -377,7 +377,7 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
             </span>
           </a>
           <ul class="treeview-menu">
-          <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_hospederia_listar.php')"><i class="fa fa-address-book-o"></i> Hospederia</a></li>
+          <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_hospederia_listar.php')"><i class="fa fa-file-text-o"></i> Hospederia</a></li>
             <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
             <li><a href = #><i class="fa fa-file-text-o"></i> Materiales</a></li>
             <li><a href = #><i class="fa fa-file-text-o"></i> Herramientas</a></li>
