faaa9b68370620767f3162ffce5208e5e366b58f
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-08-26 17:33:58 -0400

    modificacion 48

--- vista/index.php
+++ vista/index.php
@@ -496,16 +496,20 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
           </a>
         </li>
         <li> -->
-
-        <li class="active treeview">
-        <a href = #>
-            <i class="fa fa-file-text-o"></i> <span>Encuentas de opinión</span>
+        <li class="treeview">
+          <a>
+          <i class="fa fa-file-text-o"></i> <span>Encuestas de opinión</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
+          <ul class="treeview-menu">
+            <li><a href = #><i class="fa fa-file-text-o"></i>Encuestas A</a></li>
+            
+            <li><a href = #><i class="fa fa-file-text-o"></i>Encuestas B</a></li>
+          </ul>
         </li>
-        <li>
+
 
         <!--<li class="active treeview">
         <a href = #>
