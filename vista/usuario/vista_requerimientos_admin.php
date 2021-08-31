528754ffe1301be4ffe4f7e6cec576c1ba7db9da
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-08-31 12:56:20 -0400

    modificacion 57

--- vista/usuario/vista_requerimientos_admin.php
+++ vista/usuario/vista_requerimientos_admin.php
@@ -2,7 +2,7 @@
 <div class="col-md-12">
     <div class="box box-success box-solid">
         <div class="box-header with-border">
-              <h3 class="box-title">[BUSQUEDA DE REQUERIMENTOS EN EL SISTEMA] BITACORA</h3>
+              <h3 class="box-title">[REQUERIMENTOS EN EL SISTEMA]</h3>
 
             <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
