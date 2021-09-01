4547b10104b61b43fe883f37508ffe68c91a8b7d
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-09-01 13:17:59 -0400

    modificacion 64

--- vista/index.php
+++ vista/index.php
@@ -11,7 +11,7 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<link rel="icon" href="../plantilla/dist/img/ico2.ico">
+<link rel="icon" href="../plantilla/dist/img/hc.png">
   <title> Fundación|Trato Hecho Vecino|:</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
