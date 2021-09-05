7915a8e0cb1af0c25601b1351964f6fdb1009d22
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-09-04 21:26:07 -0400

    modificacion 68

--- vista/herramienta/vista_herramienta_listar.php
+++ vista/herramienta/vista_herramienta_listar.php
@@ -2,7 +2,7 @@
 <div class="col-md-12">
     <div class="box box-warning box-solid">
         <div class="box-header with-border">
-              <h3 class="box-title">[HOSPEDERIAS-ASOCIADAS] EN SISTEMA</h3>
+              <h3 class="box-title">[HERRAMIENTAS-BODEGA] EN SISTEMA</h3>
 
             <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
