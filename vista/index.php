fda074686d974cde32e933273f4a00bcdf1c40bb
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-09-04 21:56:04 -0400

    modificacion 73

--- vista/index.php
+++ vista/index.php
@@ -301,7 +301,7 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
           </a>
           <ul class="treeview-menu">
             <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_usuario_listar.php')"><i class="fa fa-address-book-o"></i> Usuarios</a></li>
-            <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
+            <li><a href = # onclick="cargar_contenido('contenido_principal','requerimiento/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
             <li><a href = #><i class="fa fa-file-text-o"></i> Materiales</a></li>
             <li><a href = # onclick="cargar_contenido('contenido_principal','herramienta/vista_herramienta_listar.php')"><i class="fa fa-file-text-o"></i> Herramientas</a></li>
             <li><a href = # onclick="cargar_contenido('contenido_principal','hospederia/vista_hospederia_listar.php')"><i class="fa fa-file-text-o"></i> Hospederia</a></li>
@@ -386,7 +386,7 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
             </span>
           </a>
           <ul class="treeview-menu">
-          <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
+          <li><a href = # onclick="cargar_contenido('contenido_principal','requerimiento/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
           <li><a href = #><i class="fa fa-file-text-o"></i> Materiales</a></li>
           <li><a href = #><i class="fa fa-file-text-o"></i> Herramientas</a></li>  
           <li><a href = # onclick="cargar_contenido('contenido_principal','hospederia/vista_hospederia_listar_t.php')"><i class="fa fa-file-text-o"></i> Hospederias</a></li> 
@@ -469,7 +469,7 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
           </a>
           <ul class="treeview-menu">
           
-            <li><a href = # onclick="cargar_contenido('contenido_principal','usuario/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
+            <li><a href = # onclick="cargar_contenido('contenido_principal','requerimiento/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
           </ul>
         </li>
 
