bec325473274ed1fd7933d21889c70436b87cb0a
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-09-13 00:48:58 -0300

    modificacion 108

--- modelo/modelo_herramienta.php
+++ modelo/modelo_herramienta.php
@@ -22,8 +22,8 @@
         }
 
         //Funcion Registrar
-        function Registrar_Material($material, $descripcion, $stock, $estatus){
-            $sql = "call SP_REGISTRAR_MATERIAL('$material', '$descripcion', '$stock', '$estatus')";
+        function Registrar_Herramienta($serial, $tipo, $marca, $modelo, $descripcion, $estatus ){
+            $sql = "call SP_REGISTRAR_HERRAMIENTA('$serial', '$tipo', '$marca', '$modelo', '$descripcion', '$estatus')";
 			if ($consulta = $this->conexion->conexion->query($sql)) {
 				if ($row = mysqli_fetch_array($consulta)) {
                         return $id= trim($row[0]); //Retorna valores 
