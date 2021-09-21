fdea6e71d31545938cbbb1d2cfd1964abf44e01f
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-09-21 13:12:21 -0300

    modificacion 124

--- vista/index.php
+++ vista/index.php
@@ -389,7 +389,7 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
           <ul class="treeview-menu">
           <!--Las pestañas que se cargaran en el menú izquierdo-->
           <li><a href = # onclick="cargar_contenido('contenido_principal','requerimiento/vista_requerimiento_listar.php')"><i class="fa fa-file-text-o"></i> Requerimientos</a></li>
-          <li><a href = #><i class="fa fa-file-text-o"></i> Materiales</a></li>
+          <li><a href = # onclick="cargar_contenido('contenido_principal','material/vista_material_listar_tr.php')"><i class="fa fa-file-text-o"></i> Materiales</a></li>
           <li><a href = # onclick="cargar_contenido('contenido_principal','herramienta/vista_herramienta_listar_tr.php')"><i class="fa fa-file-text-o"></i> Herramientas</a></li>  
           <li><a href = # onclick="cargar_contenido('contenido_principal','hospederia/vista_hospederia_listar_t.php')"><i class="fa fa-file-text-o"></i> Hospederias</a></li> 
           </ul>
