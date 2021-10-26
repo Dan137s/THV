034b81992ef153e3910a3549b1095fc6ab109fbb
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-10-25 21:22:11 -0300

    modificacion 199

--- vista/libreporte/reportes/prueba.php
+++ vista/libreporte/reportes/prueba.php
@@ -1,7 +1,14 @@
 <?php
 require_once __DIR__ .'/../vendor/autoload.php';
-require_once __DIR__ .'../../../vendor/autoload.php';
+require_once '../../../conexion_reportes/r_conexion.php';
 
+$consulta = "select * from reparacion";
+$html="Datos de la reparacion";
+$resultado = $mysqli->query($consulta);
+
+while($row = $resultado->fetch_assoc()){
+    $html.="<br>Reparacion:".$row['reparacion_id'].'';
+}
 $mpdf=new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>hello world</h1>');
-$mpdf->Output();
+$mpdf->WriteHTML($html);
+$mpdf->Output(); 
