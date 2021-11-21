0221c3f5f52ef1706a6c289ece92ccea012d1548
Daniel Alberto Ossandón Rojas <71358040+Dan137s@users.noreply.github.com>
2021-11-21 16:01:22 -0300

    modificacion 229

--- vista/libreporte/reportes/test.php
+++ vista/libreporte/reportes/test.php
@@ -1,24 +1,70 @@
 <?php
 require_once __DIR__ .'/../vendor/autoload.php';
-require_once '../../../conexion_reportes/r_conexion.php';
 
-$consulta = /**"select * from reparacion";**/
-"SELECT * FROM herramienta";
-$html="
-<table border='1'>
-    <tr>
-        <td style='border-bottom:0px solid; border-left:0px; border-right:0px; border-top:0px;'><h2 style='font-size:18px;'> Reporte & estado de herramientas</h2></td>
-    </tr>
-</table>";
+$html= ' <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Example 1</title>
+    <link rel="stylesheet" href="style.css" media="all" />
+  </head>
+  <body>
+    <header class="clearfix">
+      <div id="logo">
+        <img src="../../libreporte/css/thv.png" style="width:300px">
+        </br>
+      </div>
+      </br>
+      <h1>REPORTE DE MATERIALES</h1>
+    </header>
+    <main>
+      <table>
+        <thead>
+          <tr>
+          <th>#</th> 
+          <th>Nombre</th>
+          <th>Descripcion</th>
+          <th>Stock</th>
+          <th>Fecha Registro</th>
+          <th>Estatus</th>
+      
+          </tr>
+        </thead>
+        <tbody>';
+        require_once '../../../conexion_reportes/r_conexion.php';
+        $consulta = /**"select * from reparacion";**/
+        "SELECT * FROM material";
+        $resultado = $mysqli->query($consulta);
+        $contador=0;
+        while($row = $resultado->fetch_assoc()){
+        $contador++;
+        $html.="<br>[ID:".$row['material_id'].']' . " [Nombre:".$row['material_nombre'].']' ." [Disponible:".$row['material_stock'] .']'. " [Stock:".$row['material_estatus'].']'. " [Stock:".$row['material_estatus'].']';
 
-$resultado = $mysqli->query($consulta);
-while($row = $resultado->fetch_assoc()){
-    $html.="<br>[ID:".$row['herramienta_id'].']' . " [S/N:".$row['herramienta_serial'].']' ." [Marca:".$row['herramienta_marca'] .']'. " [Modelo:".$row['herramienta_modelo'].']'. " [Estado:".$row['herramienta_estatus'].']';
+        $html.=' <tr>
+            <td class="service">'.$contador.'</td>
+            <td class="desc">'.$row['material_nombre'].'</td>
+            <td class="unit">'.$row['material_descripcion'].'</td>
+            <td class="qty">'.$row['material_stock'].'</td>
+            <td class="total">'.$row['material_fregistro'].'</td>
+            <td class="total">'.$row['material_estatus'].'</td>';
+            
+        }
+         $html.=' </tr>
 
-
-}
-
-
-$mpdf=new \Mpdf\Mpdf(['mode' => 'utf-8']);
+        </tbody>
+      </table>
+      <div id="notices">
+        <div>NOTICE:</div>
+        <div class="notice">A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
+      </div>
+    </main>
+    <footer>
+      Invoice was created on a computer and is valid without the signature and seal.
+    </footer>
+  </body>
+</html>';
+$mpdf=new \Mpdf\Mpdf();
+$css=file_get_contents('../../libreporte/css/style.css');
+$mpdf->WriteHTML($css,1);
 $mpdf->WriteHTML($html);
 $mpdf->Output(); 
--- vista/libreporte/reportes/ticket_reporte_herramientas.php
+++ vista/libreporte/reportes/ticket_reporte_herramientas.php
@@ -1,24 +1,76 @@
 <?php
 require_once __DIR__ .'/../vendor/autoload.php';
-require_once '../../../conexion_reportes/r_conexion.php';
 
-$consulta = /**"select * from reparacion";**/
-"SELECT * FROM herramienta";
-$html="
-<table border='1'>
-    <tr>
-        <td style='border-bottom:0px solid; border-left:0px; border-right:0px; border-top:0px;'><h2 style='font-size:18px;'> Reporte & estado de herramientas</h2></td>
-    </tr>
-</table>";
+$html= ' <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" href="../../plantilla/dist/img/hc.png">
+    <title> Fundación | Trato Hecho Vecino |</title>
+    <link rel="stylesheet" href="style.css" media="all" />
+  </head>
+  <body>
+    <header class="clearfix">
+      <div id="logo">
+        <img src="../../libreporte/css/thv.png" style="width:300px">
+        </br>
+      </div>
+      </br>
+      <h1>REPORTE DE HERRAMIENTAS</h1>
+    </header>
+    <main>
+      <table>
+        <thead>
+          <tr>
+          <th class="service" style="text-align:center;">#</th>
+          <th class="service" style="text-align:center;">N° Serial</th>
+          <th class="service" style="text-align:center;">Tipo</th>  
+          <th class="service" style="text-align:center;">Marca</th>  
+          <th class="service" style="text-align:center;">Modelo</th> 
+          <th class="service" style="text-align:center;">Fecha Registro</th> 
+          <th class="service" style="text-align:center;">Descripcion</th>             
+          <th>Estatus</th>
+          </tr>
+        </thead>
+        <tbody>';
+        require_once '../../../conexion_reportes/r_conexion.php';
+        $consulta = /**"select * from reparacion";**/
+        "SELECT * FROM herramienta";
+        $resultado = $mysqli->query($consulta);
+        $contador=0;
+        while($row = $resultado->fetch_assoc()){
+        $contador++;
+        $html.="<br>[ID:".$row['herramienta_id'].']' . " [S/N:".$row['herramienta_serial'].']' ." [Marca:".$row['herramienta_marca'] .']'. " [Modelo:".$row['herramienta_modelo'].']'. " [Estado:".$row['herramienta_estatus'].']';
+        
+        $html.=' <tr>
+            <td class="service">'.$contador.'</td>
+            <td class="desc">'.$row['herramienta_serial'].'</td>
+            <td class="unit">'.$row['herramienta_tipo'].'</td>
+            <td class="qty">'.$row['herramienta_marca'].'</td>
+            <td class="total">'.$row['herramienta_modelo'].'</td>
+            <td class="total">'.$row['herramienta_fecregistro'].'</td>
+            <td class="total">'.$row['herramienta_descripcion'].'</td>
+            <td class="total">'.$row['herramienta_estatus'].'</td>';
+            
+        }
+         $html.=' </tr>
 
-$resultado = $mysqli->query($consulta);
-while($row = $resultado->fetch_assoc()){
-    $html.="<br>[ID:".$row['herramienta_id'].']' . " [S/N:".$row['herramienta_serial'].']' ." [Marca:".$row['herramienta_marca'] .']'. " [Modelo:".$row['herramienta_modelo'].']'. " [Estado:".$row['herramienta_estatus'].']';
-
-
-}
-
-
-$mpdf=new \Mpdf\Mpdf(['mode' => 'utf-8']);
+        </tbody>
+      </table>
+      <div id="notices">
+        <div>NOTICE:</div>
+        <div class="notice">A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
+      </div>
+    </main>
+    <footer>
+      Invoice was created on a computer and is valid without the signature and seal.
+      
+    </footer>
+    
+  </body>
+</html>';
+$mpdf=new \Mpdf\Mpdf();
+$css=file_get_contents('../../libreporte/css/style.css');
+$mpdf->WriteHTML($css,1);
 $mpdf->WriteHTML($html);
 $mpdf->Output(); 
